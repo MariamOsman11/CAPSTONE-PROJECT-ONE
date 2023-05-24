dd5486d7c94a7608ca5dd5be12000793c4fa0724
MariamOsman11 <mariamkca11@gmail.com>
2023-05-24 15:34:27 +0300

    Fix linters

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//mobilemenu js
+// mobilemenu js
 const navList = document.getElementById('navList');
 const closeButton = document.getElementById('closeButton');
 
@@ -14,59 +14,59 @@ function menuPopup() {
 }
 navList.addEventListener('click', menuPopup);
 
-//speakers-section
+// speakers-section
 const speakers = [{
-   id: 2,
-   image1:'images/speaker_05.png',
-   speakerName:'Manzi',
-   speakerTitle:'Ice-shop-owner',
-   speakerBio:'lorem lorem lorem lorem lorem lorem lorem lorem.',
- },
- {
-   id: 2,
-   image1:'images/speaker_01.png',
-   speakerName:'Manzi',
-   speakerTitle:'Ice-shop-owner',
-   speakerBio:'lorem lorem lorem lorem lorem lorem lorem lorem.',
- },
- {
-   id: 3,
-   image1:'images/speaker_02.png',
-   speakerName:'Ally',
-   speakerTitle:'Ice-shop-owner',
-   speakerBio:'lorem lorem lorem lorem lorem lorem lorem lorem.',
- },
- {
-   id: 4,
-   image1:'images/speaker_03.png',
-   speakerName:'Yuhu',
-   speakerTitle:'Ice-shop-owner',
-   speakerBio:'lorem lorem lorem lorem lorem lorem lorem lorem.',
- },
- {
-   id: 5,
-   image1:'images/speaker_06.png',
-   speakerName:'Alphs',
-   speakerTitle:'Ice-shop-owner',
-   speakerBio:'lorem lorem lorem lorem lorem lorem lorem lorem.',
- },
- {
-   id: 6,
-   image1:'images/speaker_05.png',
-   speakerName:'Jane',
-   speakerTitle:'Ice-shop-owner',
-   speakerBio:'lorem lorem lorem lorem lorem.',
- }];
- 
- function generateSpeakers(
-   id,
-   image1,
-   bgImg,
-   speakerName,
-   speakerTitle,
-   speakerBio,
- ) {
-   return ` 
+  id: 2,
+  image1: 'images/speaker_05.png',
+  speakerName: 'Manzi',
+  speakerTitle: 'Ice-shop-owner',
+  speakerBio: 'lorem lorem lorem lorem lorem lorem lorem lorem.',
+},
+{
+  id: 2,
+  image1: 'images/speaker_01.png',
+  speakerName: 'Manzi',
+  speakerTitle: 'Ice-shop-owner',
+  speakerBio: 'lorem lorem lorem lorem lorem lorem lorem lorem.',
+},
+{
+  id: 3,
+  image1: 'images/speaker_02.png',
+  speakerName: 'Ally',
+  speakerTitle: 'Ice-shop-owner',
+  speakerBio: 'lorem lorem lorem lorem lorem lorem lorem lorem.',
+},
+{
+  id: 4,
+  image1: 'images/speaker_03.png',
+  speakerName: 'Yuhu',
+  speakerTitle: 'Ice-shop-owner',
+  speakerBio: 'lorem lorem lorem lorem lorem lorem lorem lorem.',
+},
+{
+  id: 5,
+  image1: 'images/speaker_06.png',
+  speakerName: 'Alphs',
+  speakerTitle: 'Ice-shop-owner',
+  speakerBio: 'lorem lorem lorem lorem lorem lorem lorem lorem.',
+},
+{
+  id: 6,
+  image1: 'images/speaker_05.png',
+  speakerName: 'Jane',
+  speakerTitle: 'Ice-shop-owner',
+  speakerBio: 'lorem lorem lorem lorem lorem.',
+}];
+
+function generateSpeakers(
+  id,
+  image1,
+  bgImg,
+  speakerName,
+  speakerTitle,
+  speakerBio,
+) {
+  return ` 
      <div class="speaker-cards" title="speakers">
        <div class="each-card" id="speakerCard-${id}">
          <img class="speaker-images card2-img" src="${image1}" alt="Card2-image">
@@ -79,22 +79,20 @@ const speakers = [{
        </div>
      </div>
      `;
- }
- 
- let speakersCard = '';
- 
- speakers.forEach((index) => {
-   const speakersCardDetails = generateSpeakers(
-     index.id,
-     index.image1,
-     index.bgImg,
-     index.speakerName,
-     index.speakerTitle,
-     index.speakerBio,
-   );
-   speakersCard += speakersCardDetails;
- });
- 
- 
- document.getElementById('speaker-cards').innerHTML = speakersCard;
- 
+}
+
+let speakersCard = '';
+
+speakers.forEach((index) => {
+  const speakersCardDetails = generateSpeakers(
+    index.id,
+    index.image1,
+    index.bgImg,
+    index.speakerName,
+    index.speakerTitle,
+    index.speakerBio,
+  );
+  speakersCard += speakersCardDetails;
+});
+
+document.getElementById('speaker-cards').innerHTML = speakersCard;
